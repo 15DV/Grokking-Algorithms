24bf2e0d9de9667000df2fcc7a238e6a06d1fff6
Dmitrii Valiaev <dvalyaev15@gmail.com>
2023-01-23 22:13:07 +0300

    finish quick sort

--- src/main/java/ru/spb/ChapterFourPart2.java
+++ src/main/java/ru/spb/ChapterFourPart2.java
@@ -9,7 +9,39 @@ public class ChapterFourPart2 {
         quickSort(array, 0, array.length - 1);
     }
 
-    private void quickSort(int[] array, int startInd, int endInd) {
+    private void quickSort(int[] array, int start, int end) {
+        if (end - start < 1) {
+            return;
+        }
 
+        var pivot = array[(start + end) / 2];
+        var i = start;
+        var j = end;
+
+        while (i <= j) {
+
+            while (array[i] < pivot) {
+                i++;
+            }
+
+            while (array[j] > pivot) {
+                j--;
+            }
+
+            if (i <= j) {
+                swap(array, i, j);
+                i++;
+                j--;
+            }
+        }
+
+        quickSort(array, start, j);
+        quickSort(array, i, end);
+    }
+
+    private void swap(int[] array, int i, int j) {
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
--- src/test/java/ru/spb/ChapterFourPart2Test.java
+++ src/test/java/ru/spb/ChapterFourPart2Test.java
@@ -2,25 +2,28 @@ package ru.spb;
 
 import org.apache.commons.lang3.ArrayUtils;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.stream.Stream;
 
 class ChapterFourPart2Test {
 
     private static Stream<Arguments> provideArrays() {
         return Stream.of(
+                Arguments.of(new int[]{9, 6, 4, 2, 7, 1, 3, 5, 6}),
+                Arguments.of(new int[]{5, 3, 4, 2, 7, 1, 8, 9, 6}),
                 Arguments.of(new int[]{1, 2, 3, 4}),
                 Arguments.of(new int[]{4, 2, 3, 1}),
                 Arguments.of(new int[]{1, 2}),
                 Arguments.of(new int[]{2, 1}),
                 Arguments.of(new int[]{1, 1}),
                 Arguments.of(new int[]{1}),
-                Arguments.of(new int[]{}),
-                Arguments.of(new int[]{5, 3, 4, 2, 7, 1, 8, 9, 6}),
-                Arguments.of(new int[]{9, 6, 4, 2, 7, 1, 3, 5, 6})
+                Arguments.of(new int[]{})
         );
     }
 
